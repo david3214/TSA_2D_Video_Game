c05c74d128eda2bb4558a5d4fd9e815885f3b443
davomono <davidw3214@gmail.com>
2018-01-30 22:33:09 -0700

    basic commit

---
+++ TSA_2D_Game/Assets/Scripts/MainMenuScripts/MouseOverButtons.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseOverButtons : MonoBehaviour {
+	public Text text;
+	private int originalFontSize;
+	private Color originalColor;
+	// Update is called once per frame
+	void Awake(){
+		text = GetComponentInChildren<Text>();
+		originalColor = text.color;
+		originalFontSize = text.fontSize;
+	}
+
+	public void Grow(){
+		text.fontSize = 20;
+		text.color = Color.green;
+	}
+	public void Shrink(){
+		text.fontSize = originalFontSize;
+		text.color = originalColor;
+	}
+}
