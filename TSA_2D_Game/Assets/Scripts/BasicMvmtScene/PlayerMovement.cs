d70d234f3ac5a549a3b9efb27d0bde3d7b846371
davomono <davidw3214@gmail.com>
2018-02-08 10:07:13 -0700

    Updated the main menu screen

--- TSA_2D_Game/Assets/Scripts/BasicMvmtScene/PlayerMovement.cs
+++ TSA_2D_Game/Assets/Scripts/BasicMvmtScene/PlayerMovement.cs
@@ -6,12 +6,15 @@ public class PlayerMovement : MonoBehaviour {
 	Rigidbody2D rb;
 	public float moveSpeed = 7.0f;
 	public float jumpSpeed = 7.5f;
+	public float jumpForce = 2.0f;
+	public float slideSpeed = -3.0f;
 
 	public bool grounded = false;
+	public bool canWallJump = true;
 	public Transform groundCheck;
 	public float groundRadius = 0.2f;
 	public LayerMask whatIsGround;
-
+	public bool isJumping = false;
 	// Use this for initialization
 	void Awake () {
 		rb = GetComponent<Rigidbody2D> ();
@@ -19,6 +22,11 @@ public class PlayerMovement : MonoBehaviour {
 
 	// Update is called once per frame
 	void FixedUpdate () {
+		if (rb.velocity.y > 0) {
+			isJumping = true;
+		} else {
+			isJumping = false;
+		}
 		//grounded = Physics2D.OverlapArea (new Vector2 (	), new Vector2 (), whatIsGround);
 		movePlayer ();
 	}
@@ -37,8 +45,7 @@ public class PlayerMovement : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D other){
 		if (other.tag == "Ground") {
 			grounded = true;
-		} else if (other.tag == "Wall") {
-
+			canWallJump = true;
 		}
 	}
 	void OnTriggerExit2D(Collider2D other){
@@ -46,6 +53,17 @@ public class PlayerMovement : MonoBehaviour {
 			grounded = false;
 		}
 		else if (other.tag == "Wall") {
+
+		}
+	}
+	void OnTriggerStay2D(Collider2D other){
+		float y = Input.GetAxis ("Jump");
+		if (other.tag == "Wall" && !isJumping) {
+			rb.velocity = new Vector2 (rb.velocity.x, slideSpeed);
+		}
+		if (y > 0 && canWallJump) {
+			canWallJump = false;
+			rb.velocity = new Vector2 (-jumpForce , jumpSpeed - slideSpeed);
 
 		}
 	}
