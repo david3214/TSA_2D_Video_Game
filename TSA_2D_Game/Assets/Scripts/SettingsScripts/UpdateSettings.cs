5fc127266138fca08e052535692631f040bf0ecf
Daniel <jakoma.sms@gmail.com>
2018-03-07 23:41:05 -0700

    Added audio saving for different sessions.

---
+++ TSA_2D_Game/Assets/Scripts/SettingsScripts/UpdateSettings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateSettings : MonoBehaviour {
+
+	public Slider VolumeSlider;
+
+	// Use this for initialization
+	void Start () {
+		float PreviousValue = PlayerPrefs.GetFloat ("Volume");
+		VolumeSlider.value = PreviousValue;
+		string ValueString = Convert.ToString (PreviousValue);
+		Debug.Log (ValueString);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float VolumeLevel = VolumeSlider.value;
+		PlayerPrefs.SetFloat ("Volume", VolumeLevel);
+	}
+}
