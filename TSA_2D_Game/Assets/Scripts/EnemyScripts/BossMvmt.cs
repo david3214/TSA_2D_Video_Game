a30ba891d2233a716b0ceab53dfb44e3d5aeaa36
davomono <davidw3214@gmail.com>
2018-03-08 21:39:35 -0700

    For u Daniel

--- TSA_2D_Game/Assets/Scripts/EnemyScripts/BossMvmt.cs
+++ TSA_2D_Game/Assets/Scripts/EnemyScripts/BossMvmt.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BossMvmt : MonoBehaviour {
 	public bool CanAttack;
+	public bool startIdle;
 	public int attackNumber;
 	// Use this for initialization
 	void Start () {
@@ -12,5 +13,16 @@ public class BossMvmt : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+		
+	}
+
+	IEnumerator Attack1(){
+		yield return new WaitForSeconds(0);
+	}
+	IEnumerator Attack2(){
+		yield return new WaitForSeconds(0);
+	}
+	IEnumerator Attack3(){
+		yield return new WaitForSeconds(0);
 	}
 }
---
+++ TSA_2D_Game/Assets/Scripts/PlayerScripts/VolumeScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeScript : MonoBehaviour {
+
+	public AudioSource AS;
+	// Use this for initialization
+	void Start () {
+		AS = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		AS.volume = 1;
+	}
+}
