5047043f6c69c511520b0470593f798f236b467f
davomono <davidw3214@gmail.com>
2018-03-09 16:29:13 -0700

    Boss Idle hover

--- TSA_2D_Game/Assets/Scripts/EnemyScripts/BossMvmt.cs
+++ TSA_2D_Game/Assets/Scripts/EnemyScripts/BossMvmt.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class BossMvmt : MonoBehaviour {
 	public bool CanAttack;
-	public bool startIdle;
+	public bool idle = false;
 	public int attackNumber;
+	public float idleTime = .05f;
 	// Use this for initialization
 	void Start () {
-		
+		StartCoroutine (Idle ());
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		
+		if (!idle) {
+			
+		}
 	}
 
 	IEnumerator Attack1(){
@@ -24,5 +27,31 @@ public class BossMvmt : MonoBehaviour {
 	}
 	IEnumerator Attack3(){
 		yield return new WaitForSeconds(0);
+	}
+	IEnumerator Idle(){
+		idle = true;
+
+		for (int j = 0; j < 2; j++) {
+			float i = 0;
+			while (i < 1f) {
+				transform.position = transform.position + new Vector3 (0, .05f, 0);
+				yield return new WaitForSeconds (idleTime);
+				i += .1f;
+			}
+			i = 0;
+			while (i < 2f) {
+				transform.position = transform.position - new Vector3 (0, .05f, 0);
+				yield return new WaitForSeconds (idleTime);
+				i += .1f;
+			}
+			i = 0;
+			while (i < 1f) {
+				transform.position = transform.position + new Vector3 (0, .05f, 0);
+				yield return new WaitForSeconds (idleTime);
+				i += .1f;
+			}
+		}
+		idle = false;
+
 	}
 }
